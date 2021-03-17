56da7cc950785f68cf29ad52e774ce5d2b0e35c3
Allkman(ADev) <algirdas.cern@gmail.com>
2021-03-17 19:54:29 +0200

    Pirma uzduotis

---
+++ CSMokymai.P14.Random/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSMokymai.P14.RandomUzduotys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int metimas;
+            KauliukoMetimai(out metimas);
+            Console.WriteLine($"Metimas: {metimas}");
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+        static void KauliukoMetimai(out int metimas)
+        {
+            Random rand = new Random();
+
+            metimas = rand.Next(1, 5);
+
+            while (metimas ==1)
+            {
+                rand.Next(1, 5);          
+                
+            }
+
+            if (metimas >=2 && metimas <=4)
+            {
+                Console.WriteLine("Dėje pralaimėjote");
+            }
+            else if (metimas >= 5 && metimas <= 6 )
+            {
+                Console.WriteLine("Sveikiname, laimėjote");
+            }
+        }
+    }
+}
